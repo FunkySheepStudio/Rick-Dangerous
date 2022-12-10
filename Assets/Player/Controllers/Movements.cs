9b9e44dbe9b528e544f979a658a3244b7e3bf4c8
BartKnucle <sjarde@yahoo.fr>
2022-12-10 18:07:05 +0100

    Add Jump

--- Assets/Player/Controllers/Movements.cs
+++ Assets/Player/Controllers/Movements.cs
@@ -1,7 +1,12 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Movements : MonoBehaviour
 {
+    const float gravity = 9.8f;
+    float vSpeed = 0f;
+    float jumpSpeed = 10f;
+
     PlayerInputs inputActions;
     CharacterController characterController;
     Animator animator;
@@ -13,16 +18,43 @@ public class Movements : MonoBehaviour
         inputActions = new PlayerInputs();
         inputActions.Enable();
     }
-
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
         Move();
+        ApplyGravity();
+    }
+
+    void ApplyGravity()
+    {
+        Vector3 moves = inputActions.Movements.Move.ReadValue<Vector3>();
+        Vector3 vel = Vector3.down;
+        if (characterController.isGrounded)
+        {
+            //animator.SetBool("Grounded", true);
+            animator.SetBool("Jump", false);
+            vSpeed = 0; // grounded character has vSpeed = 0...
+            if (moves.y != 0)
+            { // unless it jumps:
+                vSpeed = jumpSpeed;
+            }
+        }
+        // apply gravity acceleration to vertical speed:
+        vSpeed -= gravity * Time.deltaTime;
+        vel.y = vSpeed; // include vertical speed in vel
+                        // convert vel to displacement and Move the character:
+
+        if (vel.y >= 0.1)
+        {
+            animator.SetBool("Jump", true);
+        }
+
+        characterController.Move(vel * Time.deltaTime);
     }
 
     void Move()
     {
         Vector3 moves = inputActions.Movements.Move.ReadValue<Vector3>();
+        Vector3 movement = Vector3.zero;
 
         if (moves.x != 0)
         {
@@ -43,11 +75,13 @@ public class Movements : MonoBehaviour
                 }
             }
 
-            characterController.Move(Vector3.right * moves.x * Time.deltaTime);
+            movement.x = moves.x;
         }
         else
         {
             animator.SetBool("Walk", false);
         }
+
+        characterController.Move(movement * Time.deltaTime);
     }
 }
